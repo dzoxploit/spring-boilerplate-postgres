b74b7dde273e53d654334a75407f1f724422e861
dzoxploit <didinnuryahya@gmail.com>
2025-03-20 10:18:57 +0700

    add yaml

---
+++ src/main/java/com/example/spring_boilerplate_stater/config/SecurityConfiguration.java
@@ -0,0 +1,5 @@
+package com.example.spring_boilerplate_stater.config;
+
+public class SecurityConfiguration {
+    
+}
---
+++ src/main/java/com/example/spring_boilerplate_stater/utils/ValidationCharacterPassword.java
@@ -0,0 +1,5 @@
+package com.example.spring_boilerplate_stater.utils;
+
+public class ValidationCharacterPassword {
+    
+}
